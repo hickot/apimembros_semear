1c6c649c3ee15d4b33251abda5743faa75e091df
hickot <bydouglascontato@gmail.com>
2024-02-07 17:11:02 -0300

    add

--- src/main/java/br/com/ministeriosemar/handlers/IllegalArgumentExceptionHandler.java
+++ src/main/java/br/com/ministeriosemar/handlers/IllegalArgumentExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.ministeriosemar.handlers;
 import java.util.ArrayList;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,14 +12,29 @@ import br.com.ministeriosemar.dtos.ErrorResponseDto;
 @ControllerAdvice
 public class IllegalArgumentExceptionHandler {
 	@ExceptionHandler(IllegalArgumentException.class) // erro que será capturado
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	//@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseStatus(HttpStatus.FORBIDDEN)
 	@ResponseBody
 	
 	public ErrorResponseDto handleIllegalArgumentException(IllegalArgumentException e) {
 		
 		ErrorResponseDto response = new ErrorResponseDto();
 		
-		response.setStatus(HttpStatus.BAD_REQUEST);
+		System.out.println("STRING DO ERRO: " + e.getMessage());
+		
+		/*
+		 * if (e.getMessage() != null &&
+		 * "Acesso negado. Usuário inválido!".equalsIgnoreCase(e.getMessage())) {
+		 * 
+		 * @ResponseStatus(HttpStatus.BAD_REQUEST)
+		 * 
+		 * 
+		 * 
+		 * }
+		 */
+		
+		//response.setStatus(HttpStatus.BAD_REQUEST);
+		response.setStatus(HttpStatus.FORBIDDEN);
 		response.setErrors(new ArrayList<String>());
 		response.getErrors().add(e.getMessage());
 		
